9b3ebf049b17825cc29a8870690a0b8b55489ca6
champloo SIXSIX <champloosixsix@gmail.com>
2024-06-08 18:08:38 -0400

    djangoReactApp

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
 import './App.css';
+import {Routes, Route} from 'react-router-dom';
+import Home from './components/Home';
+import Create from './components/Create';
+import About from './components/About';
+import Navbar from './components/Navbar';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navbar 
+        drawerWidth={220}
+        content= {      
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/create" element={<Create />} />
+            <Route path="/about" element={<About/>} />
+          </Routes>}/>
+
     </div>
   );
 }
